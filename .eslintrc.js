84ec636709db39fc2e17ae1b21ada0d9f27c7256
marcelohmdias <marcelohmdias@gmail.com>
2020-01-04 21:14:22 -0300

    :art: Adding eslint and prettier config.
    Adding config for code style.

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': isProd ? 'error' : 'off',
+    'no-debugger': isProd ? 'error' : 'off',
+    '@typescript-eslint/member-delimiter-style': 'off'
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  tabs: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
